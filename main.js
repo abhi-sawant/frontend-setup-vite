b4175f442a7a34556e642d4c08af63c1bd73a917
Abhishek <abhisawant111111@gmail.com>
2022-07-16 21:56:10 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import "./assets/scss/style.scss";
+
+let clickMe = document.getElementById("clickMe");
+let colorMask = document.getElementById("active-gradient-mask-mlbw");
+let x = 42;
+
+clickMe.addEventListener("click", function () {
+  if (x > 0) x = x - 7;
+  colorMask.children[0].setAttribute("points", `0 42, 50 42, 50 ${x}, 0 ${x}`);
+});
